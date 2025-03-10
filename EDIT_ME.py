c14b09273f83dc6b3b3cede195d2fc0b3e56fba8
Christian Fillmore <140107536+EvilPiza@users.noreply.github.com>
2025-03-10 12:23:07 -0500

    Create EDIT_ME.py

---
+++ EDIT_ME.py
@@ -0,0 +1,14 @@
+staff_channel_id = 0
+# When a user submits a form the "Accept/Deny" part will go to this channel
+
+guild_id = 0
+# To know which server its in? idk bro. it works so i mean...
+
+discord_bot_token = 0
+# You gotta make a bot from the discord developer portal then put that baby right here
+
+staff_role_ = 'STAFF'
+# Just the staff role, its the role that will be pinged when someone opens a ticket
+
+ticket_channel_category = '#TICKETS'
+# It's where the tickets will be organized, if it doesn't exist then one will be made automatically (with the name provided)
